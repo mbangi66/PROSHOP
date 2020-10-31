ac4cbdc199d55006166fed74e342fddfbe06d37a
Mustaqeem Bangi <mbangi66@gmail.com>
2020-10-31 16:09:52 +0530

    added products ,imags

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,14 @@
 import { Container } from 'react-bootstrap';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import HomeScreen from './Screens/HomeScreen';
 const App = () => {
   return (
     <>
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>Welcome to ProShop</h1>
+          <HomeScreen />
         </Container>
       </main>
       <Footer />
