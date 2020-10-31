805fa8261a61bbaa2a10a3c9f4787508c16792c1
Mustaqeem Bangi <mbangi66@gmail.com>
2020-10-31 15:19:19 +0530

    added header footer react-bootstrap fas(icons)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import { Container } from 'react-bootstrap';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 const App = () => {
-  return <div className='App'>Welcome to ProShop</div>;
+  return (
+    <>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
