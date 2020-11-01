425bc46ab6347eb81dc060b6043e1be9f81dc6a2
Mustaqeem Bangi <mbangi66@gmail.com>
2020-11-01 16:13:57 +0530

    Adding Colors To The Console (Optional)

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,13 +3,13 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL, {
-      userUnifiedTopology: true,
+      useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
     });
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline);
   } catch (error) {
-    console.log(`Error: ${error.message}`);
+    console.log(`Error: ${error.message}`.red.underline.bold);
     process.exit(1);
   }
 };
