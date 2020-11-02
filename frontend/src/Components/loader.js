8c672912276a359457171b7618b44a0ebbc4a22f
Mustaqeem Bangi <mbangi66@gmail.com>
2020-11-02 19:21:09 +0530

    Message & Loader Components

---
+++ frontend/src/Components/loader.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { Spinner } from 'react-bootstrap';
+
+export default class loader extends Component {
+  render() {
+    return (
+      <Spinner
+        animation='border'
+        role='status'
+        style={{
+          width: '100px',
+          height: '100px',
+          margin: 'auto',
+          display: 'block',
+        }}
+      >
+        <span class='sr-only'>Loading...</span>
+      </Spinner>
+    );
+  }
+}
--- frontend/src/Screens/HomeScreen.js
+++ frontend/src/Screens/HomeScreen.js
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col } from 'react-bootstrap';
 import Product from '../Components/Product';
+import Loader from '../Components/loader';
+import Message from '../Components/message';
 import { listProducts } from '../actions/productActions';
 
 const HomeScreen = () => {
@@ -17,9 +19,9 @@ const HomeScreen = () => {
     <>
       <h1>Latest Products</h1>
       {loading ? (
-        <h2>Loading...</h2>
+        <Loader />
       ) : error ? (
-        <h3>{error}</h3>
+        <Message variant='danger'>{error}</Message>
       ) : (
         <Row>
           {products.map((product) => (
